5bfed70489a36136292152ddc7111dd5af15bb8e
11by <jay3074115@gmail.com>
2024-11-19 19:15:17 +0900

    movement fix

--- Assets/Scripts/BlockMovement.cs
+++ Assets/Scripts/BlockMovement.cs
@@ -3,13 +3,24 @@ using UnityEngine;
 public class BlockMovement : MonoBehaviour
 {
     public float moveSpeed = 5f;
+    public bool gravityEnabled = false;
 
     private CameraControl cameraControl;
+
+    private Rigidbody rb;
 
     private void Start()
     {
         // CameraControl 컴포넌트 찾아서 할당
         cameraControl = FindObjectOfType<CameraControl>();
+        rb = GetComponent<Rigidbody>();
+
+        if (rb == null)
+        {
+            rb = gameObject.AddComponent<Rigidbody>();
+        }
+
+        rb.useGravity = false;
 
         if (cameraControl == null)
         {
@@ -68,14 +79,25 @@ public class BlockMovement : MonoBehaviour
         {
             if (Input.GetKey(KeyCode.RightArrow))
             {
-                movement.x += 1; // x축 이동
+                movement.x -= 1; // x축 이동
             }
             if (Input.GetKey(KeyCode.LeftArrow))
             {
-                movement.x -= 1; // x축 이동
+                movement.x += 1; // x축 이동
             }
 
             transform.position += movement * moveSpeed * Time.deltaTime;
         }
+
+        if (Input.GetKeyDown(KeyCode.Space))
+        {
+            EnableGravity();
+        }
+    }
+
+    private void EnableGravity()
+    {
+        gravityEnabled = true;
+        rb.useGravity = true;
     }
 }
